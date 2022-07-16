b8821c664fb020b97b72dde642b334eab5a4a26d
NguyenTheTan <tanhn22051999@gmail.com>
2022-07-16 21:41:43 +0700

    add page not found

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
-const routesAdmin = require("./routes/admin");
-const routesShop = require("./routes/shop");
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(routesAdmin);
-app.use(routesShop);
+app.use(adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000);
