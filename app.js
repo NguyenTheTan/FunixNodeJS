5f326af8835320451f6e119e924dd40d34bc80d0
NguyenTheTan <tanhn22051999@gmail.com>
2022-07-16 21:07:51 +0700

    change post request

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require("http");
-
 const express = require("express");
 const app = express();
 
@@ -12,7 +10,7 @@ app.use("/add-product", (req, res, next) => {
   );
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
