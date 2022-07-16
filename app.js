38aaebbb6af640649d02ece925b54f8e60d22354
NguyenTheTan <tanhn22051999@gmail.com>
2022-07-16 17:30:10 +0700

    add router

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("hello world");
+app.use("/", (req, res, next) => {
+  console.log("This alway run !");
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("I love nodejs");
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1>The Add product page</h1>");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from express.js</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
