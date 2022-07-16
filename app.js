dd767629751e48d3070f5049776a041acbe8b0eb
NguyenTheTan <tanhn22051999@gmail.com>
2022-07-16 16:52:50 +0700

    create middleware

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("hello world");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("I love nodejs");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
