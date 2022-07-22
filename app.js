24b0e9f2047f95f19e195b3671df2df2176e9076
NguyenTheTan <tanhn22051999@gmail.com>
2022-07-22 10:34:44 +0700

    add Id product on path

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const path = require("path");
-const adminRoutes = require("./routes/admin");
-const shopRouter = require("./routes/shop");
-const errorController = require("./controllers/error");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRoutes);
-app.use(shopRouter);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,18 @@
-const express = require("express");
-const path = require("path");
-const productController = require("../controllers/products");
+const path = require('path');
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const products = [];
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.get("/add-product", productController.getAddProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-router.post("/add-product", productController.postAddProduct);
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
