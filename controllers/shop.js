138e086d4ecd6b2c1f66294b6d2e520824849015
NguyenTheTan <tanhn22051999@gmail.com>
2022-08-06 21:23:01 +0700

    Add existing products and take the items in the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -76,6 +76,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
     .getCart()
     .then((cart) => {
@@ -87,8 +88,11 @@ exports.postCart = (req, res, next) => {
       if (products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
+
       if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
       return Product.findById(prodId)
         .then((product) => {
@@ -97,6 +101,11 @@ exports.postCart = (req, res, next) => {
           });
         })
         .catch((err) => console.log(err));
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
     })
     .then(() => {
       res.redirect("/cart");
