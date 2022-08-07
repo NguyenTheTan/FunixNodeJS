26b1d40124701d5037623d2f39e98d06542a7bdd
NguyenTheTan <tanhn22051999@gmail.com>
2022-08-07 21:41:59 +0700

    create connect db

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoConnect = require("./util/database");
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// const adminRoutes = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 // const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
   //   .catch((err) => console.log(err));
 });
 
-// app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes);
 // app.use(shopRoutes);
 
 app.use(errorController.get404);
