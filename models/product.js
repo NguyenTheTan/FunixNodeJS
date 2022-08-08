a074a3ad1e9128ac7a4081a423a9577aa30a073f
NguyenTheTan <tanhn22051999@gmail.com>
2022-08-08 10:10:16 +0700

    fetchall products

--- models/product.js
+++ models/product.js
@@ -1,11 +1,11 @@
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, imageUrl, price, description) {
     (this.title = title),
+      (this.imageUrl = imageUrl),
       (this.price = price),
-      (this.description = description),
-      (this.imageUrl = imageUrl);
+      (this.description = description);
   }
 
   save() {
@@ -17,6 +17,21 @@ class Product {
         console.log(result);
       })
       .catch((err) => console.log(err));
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
