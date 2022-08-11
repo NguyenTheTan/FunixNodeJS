cb58d3cea0f74210ee26402bf565a030098fc5f0
NguyenTheTan <tanhn22051999@gmail.com>
2022-08-11 19:45:25 +0700

    save data by mongoose

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const app = express();
-const User = require("./models/user");
+// const User = require("./models/user");
 const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
@@ -15,14 +15,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("62f0b482084e514e21ab3851")
-    .then((user) => {
-      req.user = new User(user.username, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("62f0b482084e514e21ab3851")
+//     .then((user) => {
+//       req.user = new User(user.username, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -31,7 +31,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://root:roottoor@cluster0.mlpal2x.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://root:roottoor@cluster0.mlpal2x.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => app.listen(3000))
   .catch((err) => {
