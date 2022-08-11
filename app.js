6fd54e7867bb83d83b4c5b37ebd85bbaedbfd5b9
NguyenTheTan <tanhn22051999@gmail.com>
2022-08-11 20:37:36 +0700

    add and use model user

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const app = express();
-// const User = require("./models/user");
+const User = require("./models/user");
 const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
@@ -15,14 +15,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("62f0b482084e514e21ab3851")
-//     .then((user) => {
-//       req.user = new User(user.username, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("62f505b2e146a10b4f561a53")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -33,7 +33,21 @@ mongoose
   .connect(
     "mongodb+srv://root:roottoor@cluster0.mlpal2x.mongodb.net/shop?retryWrites=true&w=majority"
   )
-  .then((result) => app.listen(3000))
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Max",
+          email: "max@nn.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
   .catch((err) => {
     console.log(err);
   });
