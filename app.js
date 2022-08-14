326ca4e381b707184a388df4441dfb2ca8b622ff
NguyenTheTan <tanhn22051999@gmail.com>
2022-08-14 21:11:45 +0700

    Use Middleware to handle errors correctly

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => {
-      throw new Error(err);
+      next(new Error(err));
     });
 });
 
@@ -72,7 +72,11 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render()
-  res.redirect("/500");
+  res.status(500).render("500", {
+    pageTitle: "Error",
+    path: "/505",
+    isAuthenticated: req.session.isLoggedIn,
+  });
 });
 
 mongoose
